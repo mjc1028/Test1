a10eacef251eac9f9abd61f4f57e05d800091cf3
Mekel Chapman <Mekel Chapman@DESKTOP-M0O58SC.srunet.sruad.edu>
2024-01-23 09:52:40 -0500

    Finish Hello World 3
    Close #1

--- Test/src/Elephant.java
+++ Test/src/Elephant.java
@@ -3,7 +3,10 @@ public class Elephant {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.print("Hello World");
+		System.out.print("Hello World3");
 
+		
 	}
 
 }
