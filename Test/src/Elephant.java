1c1e63ad3a8826bba2c788a902c28606196a15d3
Mekel Chapman <Mekel Chapman@DESKTOP-M0O58SC.srunet.sruad.edu>
2024-01-23 09:58:57 -0500

    Issue done
    Close #1

--- Test/src/Elephant.java
+++ Test/src/Elephant.java
@@ -4,7 +4,9 @@ public class Elephant {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Hello World");
-		System.out.print("Hello World3");
+		System.out.print("Hello World4");
+		System.out.print("Hello World5");
+
 
 		
 	}
